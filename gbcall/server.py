82fad86dc070eb30e2d58bd1063f200344cad5d3
bi1lqv <bi1lqv.y@gmail.com>
2023-02-02 21:07:56 +0800

    feat: 优化开发体验

--- gbcall/server.py
+++ gbcall/server.py
@@ -1,6 +1,10 @@
 from aiohttp import web
-from .settings import DEFAULT_PORT
-from . import handler
+if __name__=="__main__":
+    from gbcall.settings import DEFAULT_PORT
+    from gbcall import handler
+else:
+    from .settings import DEFAULT_PORT
+    from . import handler
 
 from colorama import Fore,Style
 
@@ -12,20 +16,21 @@ async def responseJsonWrapper(request,handler):
             headers={"Access-Control-Allow-Origin":"*"}
         )
     except Exception as e:
+        if __name__!="__main__":
+            raise e
         return web.json_response(
             {"res":str(e),"status":"ERR"},
             headers={"Access-Control-Allow-Origin":"*"}
         )
 
-app = web.Application(middlewares=[responseJsonWrapper])
-app.add_routes([
-    web.get('/isAlive',handler.handshake),
-    web.get('/funcRegister', handler.funcRegister),
-    web.post('/call/{funcName}', handler.callFunc),
-    web.get('/info/{funcName}', handler.getFuncInfo),
-])
-
 def run():
+    app = web.Application(middlewares=[responseJsonWrapper])
+    app.add_routes([
+        web.get('/isAlive',handler.handshake),
+        web.get('/funcRegister', handler.funcRegister),
+        web.post('/call/{funcName}', handler.callFunc),
+        web.get('/info/{funcName}', handler.getFuncInfo),
+    ])
     print(f"python_server on: {Fore.RED}http://localhost:{DEFAULT_PORT}{Style.RESET_ALL}")
     web.run_app(app, port=DEFAULT_PORT, print=None)
 
