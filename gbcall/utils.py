43a9592edac3e4aa55498598436042c4f09e23e7
bi1lqv <bi1lqv.y@gmail.com>
2023-02-02 21:34:04 +0800

    fix: input 或 output如果是空无法正确捕获

--- gbcall/utils.py
+++ gbcall/utils.py
@@ -10,10 +10,12 @@ def scanner(code):
     IOStr = re.search(regex, code).group(1)
     SignStr = re.search(regex, code).group(2)
     args = re.search(re.compile(
-        "input=\[(.+?)\]", re.S), IOStr).group(1).split(',')
+        "input=\[(.*?)\]", re.S), IOStr).group(1).split(',')
     args = list(map(lambda s: s.strip(), args))
-    ret = re.search(re.compile("output=\[(.+?)\]", re.S), IOStr).group(1).split(',')
+    args = list(filter(lambda s: s != '', args))
+    ret = re.search(re.compile("output=\[(.*?)\]", re.S), IOStr).group(1).split(',')
     ret = list(map(lambda s: s.strip(), ret))
+    ret = list(filter(lambda s: s != '', ret))
     funcName = re.search(re.compile("(.+?)\(.+?\)", re.S), SignStr).group(1)
     return funcName, args, ret
 
@@ -43,4 +45,5 @@ def typeNameConvert(typeName):
     for type in allTypes:
         if re.compile(f".*{type}$").match(typeName):
             return type
+    print(typeName)
     raise Exception("unknown type")
